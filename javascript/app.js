72ee445db7421da3bdbbdffff5984715e5ae3abe
apleek3 <apleek3@gmail.com>
2018-07-25 21:17:27 -0700

    new merge

--- javascript/app.js
+++ javascript/app.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
     console.log("Ready!"); // jquery/javascript test connection
     /*******************************************************Chart.js Test ***************************************************************/
-    var randomScalingFactor = function () {
+    var randomScalingFactor = function () { //random number generator for the demo
         return Math.round(Math.random() * 100);
     };
     $(".stats-no").empty()
 
-    var randomWalk = randomScalingFactor();
+    var randomWalk = randomScalingFactor(); //storing the random numbers as useable global variables
     var randomBike = randomScalingFactor();
     var randomTransit = randomScalingFactor();
 
-    $("#stats-no-walk").html(randomWalk);
+    $("#stats-no-walk").html(randomWalk); //adding the random numbers to the page
     $("#stats-no-bike").html(randomBike);
     $("#stats-no-transit").html(randomTransit);
 
-    $("#myChart").empty(); //empty the chart to start
-    new Chart($("#myChart"), {
+    $("#myChart").empty(); //empty the chart to start, in case there is anything in there
+    new Chart($("#myChart"), { //create new chart using chart.js
         type: 'polarArea',
         data: {
             labels: ["Walkability", "Bikeability", "Transit"],
             datasets: [
                 {
-                    label: "Percentage Relative to Other Neighborhoods",
-                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                    label: "See % Score relative to other cities below",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
                     data: [
                         randomWalk,
                         randomBike,
@@ -34,32 +34,35 @@ $(document).ready(function () {
         options: {
             title: {
                 display: true,
-                text: '% Scores Relative to the rest of the U.S.'
+                text: "See % Score relative to other cities below"
             }
         }
     });
 
-    function addData(chart, label, data) {
-        chart.data.labels.push(label);
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.push(data);
-        });
-        chart.update();
-    }
+    /*
+        function addData(chart, label, data) { //function that is used to add data to the chart when it becomes applicable
+            chart.data.labels.push(label);
+            chart.data.datasets.forEach((dataset) => {
+                dataset.data.push(data);
+            });
+            chart.update();
+        }
+     
+        function removeData(chart) { //function that is used to remove data from the chart when it becomes applicable
+            chart.data.labels.pop();
+            chart.data.datasets.forEach((dataset) => {
+                dataset.data.pop();
+            });
+            chart.update();
+        }
+        */
 
-    function removeData(chart) {
-        chart.data.labels.pop();
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.pop();
-        });
-        chart.update();
-    }
     /********************************************************Google Geocoder************************************************************ */
     var map;
     var geocoder; // global variable
     google.maps.event.addDomListener(window, 'load', initialize);
 
-    function initialize() {
+    function initialize() { //geocoder with connection to a map to be added in the near future
         geocoder = new google.maps.Geocoder();
         var mapOptions = {
             center: { lat: 42.066667, lng: -71.293056 },
@@ -74,6 +77,19 @@ $(document).ready(function () {
     // grab the search phrase from the input box
     var button = document.querySelector("#addressBtn");
     button.onclick = function () {
+        //event.preventDefault();
+        var pulledAddress = $('#address').val();
+        $("#error1").html(""); //clears out the error message
+        if (pulledAddress.length < 1) {
+            $('#error').after('<p id="error1">This field is required</p>');
+        } else {
+            var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
+            var validAddress = regEx.test(address);
+            if (!validAddress) {
+                $('#error').after('<p id="error1">Please Enter a Valid Address</p>');
+            }
+        }
+
         var locName = document.querySelector("#address").value;
         findLocation(locName);
     }
@@ -84,7 +100,6 @@ $(document).ready(function () {
             function (results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
                     console.log("found location: " + aName, results[0].geometry.location);
-
                     console.log(aName);
                     console.log(results);
                     console.log(results[0].geometry.location);
@@ -95,9 +110,8 @@ $(document).ready(function () {
                     console.log(results[0].address_components[3].long_name);
                     console.log(results[0].address_components[5].short_name);
                     console.log(results[0].address_components[7].short_name);
-
                     map.setCenter(results[0].geometry.location);
-                }
+                };
 
                 var lat = results[0].geometry.location.lat();
                 var lng = results[0].geometry.location.lng();
@@ -131,15 +145,16 @@ $(document).ready(function () {
 
                     $('#myChart').remove();
                     $('.chart-container').append('<canvas id="myChart" width="auto" height="auto"><canvas>');
+                    $("#error1").html("");
 
-                    
+
                     new Chart($("#myChart"), { //adds the Walkscore data to myChart
                         type: 'polarArea',
                         data: {
                             labels: ["Walkability", "Bikeability", "Transit"],
                             datasets: [
                                 {
-                                    label: "Percentage Relative to Other Neighborhoods",
+                                    label: "See % Score relative to other cities below",
                                     backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
                                     data: [
                                         response.walkscore,
@@ -152,13 +167,12 @@ $(document).ready(function () {
                         options: {
                             title: {
                                 display: true,
-                                text: 'Living scores relative to similar neighborhoods'
+                                text: "See % Score relative to other cities below"
                             }
                         }
-                    }); 
+                    });
 
                 });
             });
     }
-
 });
